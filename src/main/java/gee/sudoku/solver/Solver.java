b0ed4bb669af42454dd9b6a4874cd5605d954540
gerald <gerald.even@gmail.com>
2017-07-06 22:56:35 +0200

    added some testing

--- src/main/java/gee/sudoku/solver/Solver.java
+++ src/main/java/gee/sudoku/solver/Solver.java
@@ -1,15 +1,14 @@
 package gee.sudoku.solver;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Vector;
-
-import org.apache.log4j.Logger;
-
 import gee.sudoku.krn.Cell;
 import gee.sudoku.krn.Matrice;
 import gee.sudoku.krn.MatriceAction;
 import gee.sudoku.krn.MatriceZone;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
 
 /**
  * Solves the SUDOKU matrices
@@ -32,71 +31,71 @@ import gee.sudoku.krn.MatriceZone;
  * @see gee.sudoku.krn.Matrice
  */
 public class Solver {
-	private static final Logger LOG = Logger.getLogger(Solver.class);
-	Matrice mat;
-	List<Strategy> strategies = new ArrayList<Strategy>();
-	Vector<Strategies> appliedStrategies;
+    private static final Logger LOG = Logger.getLogger(Solver.class);
+    Matrice mat;
+    List<Strategy> strategies = new ArrayList<Strategy>();
+    Vector<Strategies> appliedStrategies;
 
-	private Solver() {
-		appliedStrategies = new Vector<Strategies>();
-		registerStrategies();
-	}
+    private Solver() {
+        appliedStrategies = new Vector<Strategies>();
+        registerStrategies();
+    }
 
-	public Solver(Matrice mat) {
-		this();
-		this.mat = mat;
-	}
+    public Solver(Matrice mat) {
+        this();
+        this.mat = mat;
+    }
 
-	private void registerStrategies() {
-		strategies.add(new SoleOption());
-		strategies.add(new NakedTuple(2, Strategies.NAKED_PAIR));
-		strategies.add(new NakedTuple(3, Strategies.NAKED_TRIPLET));
-		strategies.add(new NakedTuple(4, Strategies.NAKED_QUAD));
-		strategies.add(new HiddenTuple(2, Strategies.HIDDEN_PAIR));
-		strategies.add(new HiddenTuple(3, Strategies.HIDDEN_TRIPLET));
-		strategies.add(new HiddenTuple(4, Strategies.HIDDEN_QUAD));
-		strategies.add(new Candidates());
-		strategies.add(new YWing());
-		strategies.add(new XWingRow());
-		strategies.add(new XWingCol());
-		strategies.add(new SwordFish());
-	}
+    private void registerStrategies() {
+        strategies.add(new SoleOption());
+        strategies.add(new NakedTuple(2, Strategies.NAKED_PAIR));
+        strategies.add(new NakedTuple(3, Strategies.NAKED_TRIPLET));
+        strategies.add(new NakedTuple(4, Strategies.NAKED_QUAD));
+        strategies.add(new HiddenTuple(2, Strategies.HIDDEN_PAIR));
+        strategies.add(new HiddenTuple(3, Strategies.HIDDEN_TRIPLET));
+        strategies.add(new HiddenTuple(4, Strategies.HIDDEN_QUAD));
+        strategies.add(new Candidates());
+        strategies.add(new YWing());
+        strategies.add(new XWingRow());
+        strategies.add(new XWingCol());
+        strategies.add(new SwordFish());
+    }
 
-	public void loop() {
-		MatriceAction matriceAction;
-		while ((matriceAction = getNextAction()) != null) {
-			matriceAction.apply(mat);
-		}
-	}
+    public void loop() {
+        MatriceAction matriceAction;
+        while ((matriceAction = getNextAction()) != null) {
+            matriceAction.apply(mat);
+        }
+    }
 
-	public MatriceAction getNextAction() {
-		for (Strategy strategy : strategies) {
-			long start = System.currentTimeMillis();
-			MatriceAction matriceAction = strategy.getAction(mat);
-			LOG.debug(strategy.getClass().getSimpleName() + " "
-					+ (System.currentTimeMillis() - start));
-			if (matriceAction != null) {
-				return matriceAction;
-			}
-		}
-		return null;
-	}
+    public MatriceAction getNextAction() {
+        for (Strategy strategy : strategies) {
+            long start = System.currentTimeMillis();
+            MatriceAction matriceAction = strategy.getAction(mat);
+            if (matriceAction != null) {
+                LOG.info(strategy.getClass().getSimpleName() + " " + matriceAction.getStrategy() + " "
+                        + (System.currentTimeMillis() - start));
+                return matriceAction;
+            }
+        }
+        return null;
+    }
 
-	public Matrice.Status solve() throws Exception {
-		loop();
-		return mat.check();
-	}
+    public Matrice.Status solve() throws Exception {
+        loop();
+        return mat.check();
+    }
 
-	public Cell chooseCell(Matrice mat) {
-		for (MatriceZone row : mat.getRows())
-			for (Cell cell : row.getCells())
-				if (cell.getChoices().length == 2)
-					return cell;
-		return null;
-	}
+    public Cell chooseCell(Matrice mat) {
+        for (MatriceZone row : mat.getRows())
+            for (Cell cell : row.getCells())
+                if (cell.getChoices().length == 2)
+                    return cell;
+        return null;
+    }
 
-	public Vector<Strategies> getAppliedStrategies() {
-		return appliedStrategies;
-	}
+    public Vector<Strategies> getAppliedStrategies() {
+        return appliedStrategies;
+    }
 
 }
---
+++ src/test/java/gee/sudoku/solver/StrategySolverTest.java
@@ -0,0 +1,89 @@
+package gee.sudoku.solver;
+
+
+import gee.sudoku.krn.Matrice;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by gerald on 18/06/17.
+ */
+public class StrategySolverTest {
+    private static final Logger LOG = Logger.getLogger(Solver.class);
+    Matrice soleOption, nakedPair;
+    private Matrice hiddenPair;
+
+    @Before
+    public void init() {
+        soleOption = new Matrice(9);
+        soleOption.init(
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0, 1, 0, 0, 3, 7,
+                0, 8, 0, 2, 6, 3, 0, 0, 0,
+                0, 0, 4, 0, 3, 0, 0, 0, 0,
+                0, 0, 3, 9, 2, 0, 4, 0, 0,
+                0, 0, 8, 0, 0, 6, 7, 0, 0,
+                0, 3, 0, 0, 8, 1, 0, 0, 0,
+                0, 1, 5, 0, 0, 0, 9, 2, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 6);
+        nakedPair = new Matrice(9);
+        nakedPair.init(
+                9, 8, 0, 6, 0, 2, 0, 0, 3,
+                0, 0, 1, 0, 0, 0, 0, 0, 0,
+                0, 5, 0, 0, 0, 0, 0, 7, 0,
+                0, 6, 0, 0, 8, 0, 0, 0, 2,
+                8, 0, 0, 0, 0, 0, 0, 0, 0,
+                3, 0, 0, 0, 4, 0, 5, 0, 9,
+                7, 9, 0, 0, 0, 0, 0, 2, 0,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                2, 0, 0, 7, 0, 5, 0, 4, 6);
+        hiddenPair = new Matrice(9);
+        hiddenPair.init(9, 0, 7, 0, 0, 0, 2, 0, 0,
+                4, 0, 0, 9, 8, 7, 0, 0, 3,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 7, 6, 0, 0, 0, 0,
+                0, 7, 6, 2, 4, 0, 0, 1, 0,
+                5, 0, 0, 0, 1, 0, 0, 0, 8,
+                0, 0, 0, 0, 0, 0, 0, 0, 0,
+                8, 0, 0, 1, 3, 4, 0, 0, 7,
+                0, 0, 4, 0, 0, 0, 1, 0, 0);
+    }
+
+    @Test
+    public void testSolveSO() throws Exception {
+        Solver
+                solver = new Solver(soleOption);
+        solver.solve();
+        Assert.assertEquals(Matrice.Status.SOLVED, soleOption.getStatus());
+LOG.info("Solved one...");
+    }
+    @Test
+    public void testSolveNP() throws Exception {
+        Solver
+        solver = new Solver(nakedPair);
+        solver.solve();
+        Assert.assertEquals(Matrice.Status.SOLVED, nakedPair.getStatus());
+    }
+    @Test
+    public void testSolveHP() throws Exception {
+        Solver
+        solver = new Solver(hiddenPair);
+        solver.solve();
+    }
+    @Test
+    public void testSolveCBC() throws Exception {
+        Matrice sudoku = new Matrice(9);
+        sudoku.init(0, 0, 0, 0, 6, 0, 0, 7, 0, 7, 0, 1, 0, 0, 0, 2, 0,
+                4, 9, 6, 0, 0, 0, 2, 0, 3, 0, 6, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3,
+                7, 9, 0, 0, 8, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 8, 0, 6, 0,
+                5, 0, 2, 0, 0, 0, 4, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0,
+                0);
+        Solver solver = new Solver(sudoku);
+        solver.solve();
+        Assert.assertEquals(Matrice.Status.SOLVED, sudoku.getStatus());
+
+    }
+
+}
